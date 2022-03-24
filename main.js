7b818cfe7035bac639f86da9708fbbd1de51aad1
boomafromboomiland <lubomira.kovacova@avon.com>
2022-03-24 15:26:02 +0100

    Prvý postup

--- main.js
+++ main.js
@@ -7,3 +7,28 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // 	Nový pokus
+
+//globálne premenné
+
+let panacek, panacekX, panacekY, panacekSirka, panacekVyska;
+let mince, minceX, minceY, minceSirka, minceVyska;
+
+//nabehne pri loade
+function priNacitaniStranky() {
+
+}
+
+//panáčik a minca ako globálna premenná
+
+
+//šírka a výška panáčika
+
+//umiestnenie panáčika v strede
+
+//štarotová pozícia panáka
+
+//šírka a výška mince
+
+//vygenerovanie prvej mince v náhodnej pouícii
+
+//funckia, ktorá sa spustí pri stlačení klávesy
